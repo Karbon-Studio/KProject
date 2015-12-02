1eddb180f299c35472008ade06dc28db2e4c9873
Karbon-Studio <studio.karbon@gmail.com>
2015-12-02 10:05:32 +0100

    [C] suppression du contenu inutile de l' asset 2dlight

--- KarbonMVP/Assets/Scripts/Menu.cs
+++ KarbonMVP/Assets/Scripts/Menu.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Menu : MonoBehaviour {
 
+    public GameObject player;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +11,11 @@ public class Menu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float dist;
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, player.transform.position);
+        if (hit.collider.gameObject == player)
+        {
+            dist = hit.distance;
+        }
 	}
 }
