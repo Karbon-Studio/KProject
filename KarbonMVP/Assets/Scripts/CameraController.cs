d4433ed5ccf03161a5ad880c7cf5d178cd46d08b
Blake Lead <adel.abdelhak.lille@gmail.com>
2015-12-05 02:04:32 +0100

    [A] Ebauche de niveau

---
+++ KarbonMVP/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject m_Player;
+	public float m_CameraSpeed = 0.5f;
+
+	void Awake()
+	{
+		transform.position = m_Player.transform.position;
+	}
+
+	void Update()
+	{
+		Vector2 pos = Vector2.Lerp(transform.position, m_Player.transform.position + new Vector3(3, 0, 0), m_CameraSpeed * Time.deltaTime);
+		transform.position = new Vector3(pos.x, pos.y, -10);
+	}
+	
+}
