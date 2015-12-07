3c1f1528f3eb1d471e94dc003902f84cd26d1783
Karbon-Studio <studio.karbon@gmail.com>
2015-12-07 16:03:25 +0100

    [C] suppression des redondances

---
+++ Clement/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject m_Player;
+	public float m_CameraSpeed = 0.5f;
+	private int m_Direction = 1;
+
+	void Awake()
+	{
+		transform.position = m_Player.transform.position;
+	}
+
+	void Update()
+	{
+		Vector2 pos = Vector2.Lerp(transform.position, m_Player.transform.position + new Vector3(3 * m_Direction, 0, 0), m_CameraSpeed * Time.deltaTime);
+		transform.position = new Vector3(pos.x, pos.y, -10);
+	}
+	
+}
