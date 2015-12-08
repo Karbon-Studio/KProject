ab24d22402a1baccc2816107e77aba8f4701321e
Karbon-Studio <studio.karbon@gmail.com>
2015-12-08 14:43:17 +0100

    [V] Perso v0.01

--- Valkp/Assets/Scripts/Accroche.cs
+++ Valkp/Assets/Scripts/Accroche.cs
@@ -5,35 +5,10 @@ public class Accroche : MonoBehaviour {
 
     public Transform groundCheck;
     public LayerMask whatIsGround;
-    public GameObject player;
     public GameObject handCollider;
     private float groundRadius = 0.2f;
     private bool grounded = false;
-    public float jumpForce;
-
-    private void Awake()
-    {
-    }
-
-  /*  void OnTriggerStay2D (Collider2D other)
-    {
-        if (other.gameObject.tag == "Rebord")
-        {
-            if (Input.GetButtonDown("Use/Suspend"))
-            {
-                GetComponent<Collider2D>().isTrigger = false;
-            }
-        }
-    }*/
-
-    void OnCollisionStay2D(Collision2D other)
-    {
-        if (Input.GetButtonDown("Jump") && (other.gameObject.tag == "Rebord"))
-        {
-            PlayerControllerScripts playerJ;
-            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
-        }
-    }
+    
 
     void FixedUpdate()
     {
