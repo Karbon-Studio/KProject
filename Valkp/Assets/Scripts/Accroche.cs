f5024a171acc02575da14f509fe0b904d15c18c0
Karbon-Studio <studio.karbon@gmail.com>
2015-12-08 11:02:01 +0100

    [C] importation du porjet de valentin

---
+++ Valkp/Assets/Scripts/Accroche.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accroche : MonoBehaviour {
+
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public GameObject player;
+    public GameObject handCollider;
+    private float groundRadius = 0.2f;
+    private bool grounded = false;
+    public float jumpForce;
+
+    private void Awake()
+    {
+    }
+
+  /*  void OnTriggerStay2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Rebord")
+        {
+            if (Input.GetButtonDown("Use/Suspend"))
+            {
+                GetComponent<Collider2D>().isTrigger = false;
+            }
+        }
+    }*/
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (Input.GetButtonDown("Jump") && (other.gameObject.tag == "Rebord"))
+        {
+            PlayerControllerScripts playerJ;
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        if (grounded)
+        {
+            handCollider.SetActive(false);
+        }
+        if (Input.GetButtonDown("Use/Suspend"))
+        {
+            handCollider.SetActive(true);
+        }
+        else if (Input.GetButtonUp("Use/Suspend"))
+        {
+            handCollider.SetActive(false);
+        }
+    }
+}
---
+++ Valkp/Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dist;
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, player.transform.position);
+        if (hit.collider.gameObject == player)
+        {
+            dist = hit.distance;
+        }
+	}
+}
