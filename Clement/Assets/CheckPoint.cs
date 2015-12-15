30f18070a932be1fffaa0fd1f4e475de7c5c130c
Karbon-Studio <studio.karbon@gmail.com>
2015-12-15 09:32:56 +0100

    [C] Checkpoint + level
    creation du checkpoint light (script pas fini) et du level via
    tiledEditor

---
+++ Clement/Assets/CheckPoint.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : MonoBehaviour
+{
+
+    public bool checkpoint = false;
+    public bool recupHeal = false;
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == player.tag)
+        {
+            checkpoint = true;
+            recupHeal = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == player.tag)
+        {
+            checkpoint = false;
+            recupHeal = false;
+        }
+
+    }
+}
