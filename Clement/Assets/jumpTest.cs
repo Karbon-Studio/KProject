cb533dbfdd9784f9edf4ad9f294eed14c27f33a7
Karbon-Studio <studio.karbon@gmail.com>
2015-12-16 15:29:23 +0100

    [C] Creation test saut et chute verticale

--- Clement/Assets/jumpTest.cs
+++ Clement/Assets/jumpTest.cs
@@ -1,30 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class jumpTest : MonoBehaviour
 {
     public GameObject player;
     public float y;
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
+    public bool canJump = true;
 
     // Update is called once per frame
     void Update()
     {
         float move = Input.GetAxis("Horizontal");
-
-        
         player.GetComponent<Rigidbody2D>().velocity = new Vector2(move * 5, player.GetComponent<Rigidbody2D>().velocity.y);
-        Vector2 jump = new Vector2(0, Input.GetAxis("Vertical"));
+        Vector2 jump = new Vector2(0, 30 /** Input.GetAxis("Vertical")*/);
         y = jump.y;
-        if(Input.GetButton("Jump"))
+        if(Input.GetButton("Jump") && canJump)
         {
             player.GetComponent<Rigidbody2D>().AddForce(jump);
-
+            //canJump = false;
+            StartCoroutine(JumpPower());
         }
+        if(!canJump && (player.GetComponent<Rigidbody2D>().velocity.y == 0))
+        {
+            StartCoroutine(DelayJump());
+        }
+    }
+
+    private IEnumerator JumpPower()
+    {
+        yield return new WaitForSeconds(0.3f);
+        canJump = false;
+    }
+
+    private IEnumerator DelayJump()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canJump = true;
     }
 }
